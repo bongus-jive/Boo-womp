a8e78ca855a340d2c607b20f9cf8d53c374dd449
patmanf <27874300+patmanf@users.noreply.github.com>
2021-09-21 21:20:00 -0500

    forgot to remove a logInfo

--- pat/boowomp.lua
+++ pat/boowomp.lua
@@ -11,15 +11,12 @@ end
 
 function update(dt)
 	for _,v in ipairs(world.entityPortrait(player.id(), "head")) do
-		sb.logInfo(v.image)
-		
 		if string.find(v.image, "emote.png:") then
 			local sad = string.find(v.image, "emote.png:sad.")
 			
 			if sad then
 				if not boowomped then
 					boowomped = true
-					
 					player.interact("ScriptPane", {gui = {}, scripts = {"/pat/boowomp.lua"}})
 				end
 			else
